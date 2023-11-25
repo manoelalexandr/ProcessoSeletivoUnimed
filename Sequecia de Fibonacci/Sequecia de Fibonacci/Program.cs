965b0393067af6865db9628ac39393e7856208f1
manoelalexandr <65196600+manoelalexandr@users.noreply.github.com>
2023-11-25 18:51:31 -0300

    Add files via upload

---
+++ Sequecia de Fibonacci/Sequecia de Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿class program
+{
+    static void Main()
+    {
+        // Mostra o resultado
+        foreach (int numero in GerarFibonacci(15))
+        {
+            Console.WriteLine(numero);
+        }
+    }
+
+    // A função já gera os 2 primeiros números da sequência para que seja possível dar inicio 
+    // a sequencia, somando os dois anteriores para gerar o número 
+    static int[] GerarFibonacci(int quantidade)
+    {
+        int[] numeros = new int[quantidade];
+
+        // Dois números da sequência
+        numeros[0] = 0;
+        numeros[1] = 1;
+
+        // Gerando os números
+        for (int i = 2; i < quantidade; i++)
+        {
+            numeros[i] = numeros[i - 1] + numeros[i - 2];
+        }
+
+        return numeros;
+    }
+}
